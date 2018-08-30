03ad64cf11a92197a56369d81f27c6eb6cd7b452
mossgrammer <mossgrammer@gmail.com>
2018-08-30 22:23:02 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'SLgSZ1xW3NmZDNUnqllgqlRxX6+G+eyIDtBoW6+RkhtxteLwaFdzY35smLNCNZwHVQCRVwV+7n5e0oE37997+lBj1fAGP+cru6OwtKkEhfyWZk6eB8BCu/caT7g+efEI03PfEEc+busaZleFEwykKgdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
